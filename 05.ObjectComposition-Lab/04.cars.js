5bb894c6ae7a7f45ca69106732ecf7b99a9cab04
Videlin Donchev <ya.5164646777752015@yandex.ru>
2016-10-31 11:16:26 +0200

    simplified solution

--- 05.ObjectComposition-Lab/04.cars.js
+++ 05.ObjectComposition-Lab/04.cars.js
@@ -16,17 +16,13 @@ function cars(commands) {
         }
 
         function print(objName) {
+            let res = [];
             let obj = allCars.get(objName);
-            let objKeys = Object.keys(allCars.get(objName))
-                .map(k => `${k}:${allCars.get(objName)[k]}`);
-
-            let proto = obj;
-            while (proto = Object.getPrototypeOf(proto)) {
-                let o =Object.keys(proto).map(k => `${k}:${proto[k]}`);
-                objKeys = objKeys.concat(o)
+            for (let prop in obj) {
+                res.push(`${prop}:${obj[prop]}`);
             }
 
-            console.log(objKeys.join(', '));
+            console.log(res.join(', '));
         }
 
         return {create, set, print};
@@ -38,13 +34,3 @@ function cars(commands) {
         carCommandInterpreter[commandName](...tokens);
     }
 }
-
-cars(['create pesho','create gosho inherit pesho','create stamat inherit gosho','set pesho rank number1','set gosho nick goshko','print stamat']);
-
-// cars(['create c1',
-//     'create c2 inherit c1',
-//     'set c1 color red',
-//     'set c2 model new',
-//     'print c1',
-//     'print c2']
-// );
